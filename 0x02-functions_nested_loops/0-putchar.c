f21f6b2a8b9d9d6da3b71a9c0ab3a70d7bd7a83a
Emmanuel Odewale <odebzemmygee@gmail.com>
2022-09-14 00:57:59 +0100

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return 0 Always Success
+ * 
+ */
+ int main(void)
+ {
+    char c;
+    c = "_putchar";
+
+    printf("%c\n",c);
+    return (0);
+ }
